d583be50284faf75ce24c38b8913fedf5e8ebdb9
Pavel <pavelfedorov929@gmail.com>
2021-08-19 00:26:00 +0300

    стадия правок

--- src/js/script.js
+++ src/js/script.js
@@ -16,17 +16,17 @@ $(function () {
     $('.nav__arrow-btn').on('click', function () {
         $('.nav').slideUp('300');
     });
-    /* const tooltipCloseBtn = document.querySelectorAll('.tooltip__close-btn');
-    for (let i = 0; i < tooltipCloseBtn.length; i++) {
-        tooltipCloseBtn[i].addEventListener('click', function () {
-            this.parentNode.style.cssText = 'opacity: 0; visibility: hidden';
-        });
-    } */
     $('.tooltip__btn').hover(function () {
         $(this).parent().find('div.tooltip__info').toggleClass('tooltip__info--active');
     });
     $('.tooltip__close-btn').on('click', function () {
         $(this).parent().removeClass('tooltip__info--active');
+    });
+
+    $("a[href^='#']").click(function () {
+        const href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(href).offset().top + "px" });
+        return false;
     });
 
 
@@ -45,6 +45,12 @@ $(function () {
     $('input[name="target_retail"]').on('change', function () {
         console.log($(this).val());
         $('#target-retail-value').text($(this).val()+'$');
+    });
+    $('#basic_name').on('change', function () {
+        $('#contact_name').val($(this).val());
+    });
+    $('#basic_email').on('change', function () {
+        $('#contact_email').val($(this).val());
     });
     $('input[name="name"]').on('change', function () {
         console.log($(this).val());
@@ -126,28 +132,13 @@ $(function () {
 
     function getCheckboxValues(parent, value) {
         let checkboxInputs = parent.querySelectorAll('input[type="checkbox"]');
-        //let checked = [];
         for (let i = 0; i < checkboxInputs.length; i++) {
-            /* if (checkboxInputs[i].value == 'Other (type your answer)') {
-                console.log(checkboxInputs[i].value);
-                checked.pop(checkboxInputs[i].value);
-                console.log(checked);
-                let otherInput = checkboxInputs[i].parentNode.nextElementSibling;
-                otherInput.addEventListener('input', function () {
-                    console.log(otherInput.value);
-                    checked.push(otherInput.value);
-                    value.innerHTML = otherInput.value;
-                    console.log(value);
-                });
-            } */
             if (checkboxInputs[i].checked) {
                 value.innerHTML = checkboxInputs[i].value;
-                console.log(value);
             }
             checkboxInputs[i].addEventListener('change', () => {
                 if (checkboxInputs[i].checked) {
                     value.innerHTML += '<br>' + checkboxInputs[i].value;
-                    console.log(value);
                     if (checkboxInputs[i].value == 'Other (type your answer)') {
                         let otherInput = checkboxInputs[i].parentNode.nextElementSibling;
                         otherInput.addEventListener('change', function () {
@@ -181,34 +172,49 @@ $(function () {
     getCheckboxValues(processRole, processRoleValue);
 
 
+    $('input[value="Wireless"]').on('change', function () {
+        if ($(this).is(':checked')) {
+            $('.step-item__subchecks').slideDown('300');
+        } else {
+            $('.step-item__subchecks').slideUp('300');
+        }
+    });
+
+
     // Логика шагов и прогресс
     let currentStep = 0;
     const stepImg = document.querySelector('#step-img');
     const stepImgSrc = ['img/watches/1.svg', 'img/watches/2.svg', 'img/watches/3.svg', 'img/watches/4.svg', 'img/watches/5.svg', 'img/watches/6.png', 'img/watches/7.png', 'img/watches/8.png'];
     const stepPercents = [12.5, 25, 27.5, 50, 62.5, 75, 87.5, 100];
-    const statusPercent = document.querySelector('.percent');
+    const statusPercent = document.querySelector('.nav__status-percent');
     const statusMobPercent = document.querySelector('.top-nav__progress-percent');
     const stepTitle = document.querySelector('.top-nav__title');
-    console.log(stepTitle);
     const navMenuItems = document.querySelectorAll('.nav__list-item');
-    console.log(navMenuItems);
     const statusText = document.querySelector('.nav__status');
-    console.log(statusText);
     let stepItems = document.querySelectorAll('.step__content');
     let backBtn = document.querySelector('.back-btn');
     let nextBtn = document.querySelector('.next-btn');
-    const editButtons = document.querySelectorAll('.edit-btn');
-    console.log(editButtons);
+    //const editButtons = document.querySelectorAll('.edit-btn');
+    let main = document.querySelector('main');
+    console.log(main);
+    let thanks = document.querySelector('.thanks');
+    console.log(thanks);
+
     backBtn.onclick = () => prevStep(1);
     nextBtn.onclick = () => nextStep(1);
     showStep(currentStep);
 
     function showStep(n) {
-        stepItems[n].style.display = "block";
-        if (n == 0) {
+        if (n <= 7) {
+            stepItems[n].style.display = "block";
+        }
+        if (n == 0 || n == 7) {
             backBtn.style.display = "none";
+        } else if (n == 8) {
+            $('main').hide('300');
+            $('.thanks').fadeIn('300');
         } else {
-            backBtn.style.display = "block";
+            backBtn.style.display = "flex";
         }
     }
 
@@ -234,16 +240,13 @@ $(function () {
         } else if (n == 7) {
             navMenuItems[n - 1].classList.remove('nav__list-item--done');
             navMenuItems[n - 1].classList.add('nav__list-item--current');
-            //statusText.classList.remove('nav__status--completed');
+            statusText.classList.remove('nav__status--completed');
         } else {
             navMenuItems[n + 1].classList.remove('nav__list-item--current');
             navMenuItems[n].classList.remove('nav__list-item--done');
             navMenuItems[n].classList.add('nav__list-item--current');
             statusText.classList.remove('nav__status--completed');
         }
-        /* if (n !== 7) {
-            navMenuItems[n + 1].classList.remove('nav__list-item--current');
-        } */
     }
     showActiveMenuItemPrev(currentStep);
 
@@ -254,9 +257,10 @@ $(function () {
     showPercent(currentStep);
 
     function fillStepTitle(n) {
-        stepTitle.textContent = navMenuItems[n].textContent;
         if (n == 7) {
             stepTitle.textContent = navMenuItems[n - 1].textContent;
+        } else {
+            stepTitle.textContent = navMenuItems[n].textContent;
         }
     }
     fillStepTitle(currentStep);
@@ -266,31 +270,31 @@ $(function () {
     }
     showStepImg(currentStep);
 
-    function editStep() {
+    /* function editStep() {
         for (let i = 0; i < editButtons.length; i++) {
             editButtons[i].addEventListener('click', function () {
-                stepItems[editButtons.length].style.display = 'none';
                 showStep(i);
             });
         }
     }
-    editStep();
+    editStep(); */
 
-    function navigateSteps() {
+    /* function navigateSteps() {
         for (let i = 0; i < navMenuItems.length; i++) {
             navMenuItems[i].addEventListener('click', function () {
-                //stepItems[navMenuItems.length].style.display = 'none';
                 showStep(i);
             });
         }
     }
-    navigateSteps();
+    navigateSteps(); */
 
     function nextStep(n) {
         if (n == 1 && !validateForm()) return false;
-        stepItems[currentStep].style.display = "none";
-        currentStep = currentStep + n;
-        console.log(currentStep);
+        if (n <= 7) {
+            stepItems[currentStep].style.display = "none";
+            currentStep = currentStep + n;
+        }
+        //console.log(currentStep);
         showStep(currentStep);
         showStepImg(currentStep)
         showActiveMenuItemNext(currentStep);
